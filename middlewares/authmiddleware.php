4577437ade89e482196e9ecc4f2a21447755e42a
Kgp31 <146392176+Kgp31@users.noreply.github.com>
2024-12-01 23:19:40 -0600

    Middleware
    Middleware completo y funcional

--- middlewares/authmiddleware.php
+++ middlewares/authmiddleware.php
@@ -1,9 +1,11 @@
 <?php
-session_start();
-
+// Función para verificar la autenticación del usuario
 function checkAuth() {
+    // Verificar si la sesión no tiene un nombre de usuario establecido
     if (!isset($_SESSION['username'])) {
-        header("Location: /inventariomizaki/public/login.html"); // Ruta ajustada para asegurar que es correcta
+        // Redirigir al usuario a la página de inicio de sesión si no está autenticado
+        header("Location: http://localhost:8888/public/login.html");
+        // Terminar la ejecución del script para asegurar que no se ejecute más código
         exit();
     }
 }
